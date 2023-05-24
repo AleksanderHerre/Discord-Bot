f477cffa753e26d1fadf7c462b7b4e3665abd9b4
AleksanderHerre <112162882+AleksanderHerre@users.noreply.github.com>
2023-05-24 23:12:22 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,7 +15,7 @@ async def send_message(message, user_message, is_private):
 
 
 def run_discord_bot():
-    TOKEN = 'MTA3MTAzNDU4NDE0MzI0OTQxOQ.G_zfvy.WIvvCW_5d7Fg4mL9e_srTbymYRK-vXObJ8kcjU'
+    TOKEN = 'TOKEN_NAME HERE!'
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents=intents)
